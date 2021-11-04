6d4432a38a5353183b6dd9a400a8607a84449004
EducatedStrikeCart <dhn004@gmail.com>
2021-11-04 19:15:21 -0400

    studio fin

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            if (ViewBag.passwordVerified == null)
+            {
+                ViewBag.passwordVerified = true;
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user/add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            ViewBag.Username = newUser.Username;
+            ViewBag.Email = newUser.Email;
+
+            if (newUser.VerifyPassword(verify))
+            {
+                ViewBag.passwordVerified = true;
+                ViewBag.TimeCreated = newUser.TimeCreated;
+                return View("Index");
+            }
+
+            ViewBag.passwordVerified = false;
+            return View("Add");
+
+
+        }
+    }
+}
+
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public DateTime TimeCreated { get; set; }
+
+        public User() { }
+
+        public User(string username, string email, string password, DateTime temp)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+            TimeCreated = temp;
+        }
+
+        public User(string username, string email, string password):this(username, email, password, DateTime.Now)
+        {
+
+        }
+
+
+        public Boolean VerifyPassword(string verify)
+        {
+            return Password == verify;
+        }
+
+        public DateTime GetTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
